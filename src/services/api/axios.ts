f724fd4880a6afecd1ff20abfdfbc7447e0ece87
JuniorDilmurodov9979 <dilmurodovjr@gmail.com>
2026-02-27 17:44:42 +0500

    inital commit

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 // VITE_APP_BASE_URL real API
 const api = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL,
+  baseURL: "https://loyiha.kuprikqurilish.uz/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
